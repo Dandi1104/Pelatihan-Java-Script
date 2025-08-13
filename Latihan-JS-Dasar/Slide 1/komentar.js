d28492018243e2d87339ce45771e78a2c2ad147c
Dandi1104 <ramadhandandi502@gmail.com>
2025-08-13 14:49:11 +0700

    Delete Latihan-JS-Dasar directory

--- Latihan-JS-Dasar/Slide 1/komentar.js
+++
@@ -1,9 +0,0 @@
-// Komentar satu baris
-
-/*
-  Komentar beberapa baris
-  Menjelaskan bagian program
-*/
-
-let nama = "Dandi Nur Ramadhan"; // Menyimpan nama siswa
-console.log("Nama saya", nama); // Tampilkan ke layar
